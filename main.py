62f86fd3f2330f62e9892c105eddeed5dc3738af
Rajkumar S <rajkumaar2304@icloud.com>
2021-06-02 10:33:16 +0530

    accept date as parameter

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup
+from datetime import date
 import requests
+import getopt
+import sys
 
-page = requests.get("https://news.ycombinator.com/front?day=2021-06-01")
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, '', ['date='])
+
+today = date.today()
+for (opt, value) in opts:
+    if opt == "--date":
+        today = value
+
+page = requests.get("https://news.ycombinator.com/front?day=" + today)
 soup = BeautifulSoup(page.content, 'html.parser')
 items = soup.find_all('tr', class_="athing")[:10]
 digest = []
@@ -11,7 +22,11 @@ for item in items:
     story = item.find('a', class_="storylink")
     title = story.text
     href = story['href']
-    domain = getattr(item.find('span', class_="sitestr"), 'text', 'news.ycombinator.com')
+    domain_item = item.find('span', class_="sitestr")
+    if domain_item:
+        domain = domain_item.text
+    else:
+        domain = 'news.ycombinator.com'
     points = soup.find('span', id=("score_" + id)).text
     digest.append({
         'title': title,
